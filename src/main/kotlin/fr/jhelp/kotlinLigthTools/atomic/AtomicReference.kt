7969f0cea12761cadb68348a9122c7bb1325f010
JHelp <jhelpgg@gmail.com>
2020-09-29 21:29:16 +0200

    Atomics

---
+++ src/main/kotlin/fr/jhelp/kotlinLigthTools/atomic/AtomicReference.kt
@@ -0,0 +1,54 @@
+package fr.jhelp.kotlinLigthTools.atomic
+
+import fr.jhelp.kotlinLight.Mutex
+
+class AtomicReference<T>
+{
+    private val mutex: Mutex
+    private var value: T?
+
+    constructor(value: T?)
+    {
+        this.mutex = Mutex()
+        this.value = value
+    }
+
+    fun get(): T?
+    {
+        var value: T? = null
+        this.mutex.safeExecute { value = this.value }
+        return value
+    }
+
+    fun set(value: T?)
+    {
+        this.mutex.safeExecute { this.value = value }
+    }
+
+    fun getAndSet(newValue: T?): T?
+    {
+        var value: T? = null
+
+        this.mutex.safeExecute {
+            value = this.value
+            this.value = newValue
+        }
+
+        return value
+    }
+
+    fun compareAndSet(excepted: T?, newValue: T?): Boolean
+    {
+        var done = false
+
+        this.mutex.safeExecute {
+            if (this.value == excepted)
+            {
+                this.value = newValue
+                done = true
+            }
+        }
+
+        return done
+    }
+}
